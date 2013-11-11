0b028b9effd1f48068339b4cc8d4bb14b068293c
Nick Palombo <npalombo@gmail.com>
2013-11-11 13:56:45 -0600

    Previous changes got stomped. UID is read only and always present. No need to set.
    Signed-off-by: Nick Palombo <npalombo@gmail.com>

--- client/jobsub_remote.sh
+++ client/jobsub_remote.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 INPUT=`echo "$@"`
-UID=`id -u`
-COMMAND=`echo $INPUT | base64 -w 0`
-curl -cert /tmp/x509up_u${UID}.p12 -k -X POST -d -jobsub_args_base64=$COMMAND https://fermicloud326.fnal.gov:8443/jobsub/experiments/1/jobs/ && echo
+
+BASE64="base64 -w 0"
+if [[ `uname` == 'Darwin' ]]; then
+    BASE64="base64"
+fi
+COMMAND=`echo $INPUT | $BASE64`
+
+# Find arguments starting with @. Assume it is a file that will be transferred
+# TODO: Do we want to upload multiple files?
+for arg in "$@"
+do
+    if [[ "$arg" == @* ]]
+    then
+        file_upload="-F jobsub_command=$arg"
+        echo "File ${arg:1} will be uploaded"
+    fi
+done
+
+curl -cert /tmp/x509up_u${UID} -k -X POST $file_upload -F jobsub_args_base64=$COMMAND https://fermicloud326.fnal.gov:8443/jobsub/experiments/1/jobs/ && echo

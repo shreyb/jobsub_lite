41d53d59782ec64ec4b4138d99589207957a7436
Nick Palombo <npalombo@gmail.com>
2013-11-12 19:34:26 -0600

    Getting the user
    Signed-off-by: Nick Palombo <npalombo@gmail.com>

--- server/jobsub_api.py
+++ server/jobsub_api.py
@@ -35,56 +35,62 @@ class JobsResource(object):
     @cherrypy.expose
     def index(self, exp_id, job_id=None, **kwargs):
         try:
-            if cherrypy.request.method == 'POST':
-                if job_id is None:
-                    cherrypy.request.app.log.error('kwargs: ' + str(kwargs))
-                    jobsub_args = kwargs.get('jobsub_args_base64')
-                    if jobsub_args is not None:
-                        jobsub_args = base64.b64decode(jobsub_args).rstrip()
-                        cherrypy.request.app.log.error('jobsub_args: ' + str(jobsub_args))
-                        jobsub_command = kwargs.get('jobsub_command')
-                        cherrypy.request.app.log.error('jobsub_command: ' + str(jobsub_command))
-                        if jobsub_command is not None:
-                            # TODO: get the command path root from the configuration
-                            command_path_root = '/opt/jobsub/uploads/'
-                            # TODO: create sub directories for the user and request id or timestamp
-                            mkdir_p(command_path_root)
-                            command_path = os.path.join(command_path_root, jobsub_command.filename)
-                            cherrypy.request.app.log.error('command_path: ' + str(command_path))
-                            with open(command_path, 'wb') as dst_file:
-                                copyfileobj(jobsub_command.file, dst_file)
-                            # replace the command file name in the arguments with the path on the local machine
-                            command_tag = '@(.*)%s' % jobsub_command.filename
-                            jobsub_args = re.sub(command_tag, command_path, jobsub_args)
-                            cherrypy.request.app.log.error('jobsub_args (subbed): ' + str(jobsub_args))
+            user = cherrypy.request.headers.get('Auth-User')
+            if user is not None:
+                cherrypy.request.app.log.error('user: %s' % user)
+                if cherrypy.request.method == 'POST':
+                    if job_id is None:
+                        cherrypy.request.app.log.error('kwargs: %s' % str(kwargs))
+                        jobsub_args = kwargs.get('jobsub_args_base64')
+                        if jobsub_args is not None:
+                            jobsub_args = base64.b64decode(jobsub_args).rstrip()
+                            cherrypy.request.app.log.error('jobsub_args: %s' % jobsub_args)
+                            jobsub_command = kwargs.get('jobsub_command')
+                            cherrypy.request.app.log.error('jobsub_command: %s' + jobsub_command)
+                            if jobsub_command is not None:
+                                # TODO: get the command path root from the configuration
+                                command_path_root = '/opt/jobsub/uploads/'
+                                # TODO: create sub directories for the user and request id or timestamp
+                                mkdir_p(command_path_root)
+                                command_path = os.path.join(command_path_root, jobsub_command.filename)
+                                cherrypy.request.app.log.error('command_path: %s' % command_path)
+                                with open(command_path, 'wb') as dst_file:
+                                    copyfileobj(jobsub_command.file, dst_file)
+                                # replace the command file name in the arguments with the path on the local machine
+                                command_tag = '@(.*)%s' % jobsub_command.filename
+                                jobsub_args = re.sub(command_tag, command_path, jobsub_args)
+                                cherrypy.request.app.log.error('jobsub_args (subbed): %s' % jobsub_args)
 
-                        jobsub_args = jobsub_args.split(' ')
-                        #TODO: the path to the jobsub tool should be configurable
-                        command = ['/opt/jobsub/jobsub_env_runner.sh'] + jobsub_args
-                        cherrypy.request.app.log.error('command: ' + str(command))
-                        pp = Popen(command, stdout=PIPE, stderr=PIPE)
-                        result = {
-                            'out': pp.stdout.readlines(),
-                            'err': pp.stderr.readlines()
-                        }
-                        return str(json.dumps(result))
+                            jobsub_args = jobsub_args.split(' ')
+                            #TODO: the path to the jobsub tool should be configurable
+                            command = ['/opt/jobsub/jobsub_env_runner.sh'] + jobsub_args
+                            cherrypy.request.app.log.error('command: %s' % command)
+                            pp = Popen(command, stdout=PIPE, stderr=PIPE)
+                            result = {
+                                'out': pp.stdout.readlines(),
+                                'err': pp.stderr.readlines()
+                            }
+                            return str(json.dumps(result))
+                        else:
+                            #TODO: return an error because no command was supplied
+                            pass
                     else:
-                        #TODO: return an error because no command was supplied
+                        #TODO: return an error because job_id has been supplied but POST is for creating new jobs
                         pass
-                else:
-                    #TODO: return an error because job_id has been supplied but POST is for creating new jobs
-                    pass
-            elif cherrypy.request.method == 'GET':
-                if job_id is not None:
-                    job_id = int(job_id)
-                    schedd = condor.Schedd()
-                    results = schedd.query()
-                    for job in results:
-                        if job['ClusterId'] == job_id:
-                            return str(job)
-                else:
-                    #TODO: return an error because job_id has not been supplied but GET is for querying jobs
-                    pass
+                elif cherrypy.request.method == 'GET':
+                    if job_id is not None:
+                        job_id = int(job_id)
+                        schedd = condor.Schedd()
+                        results = schedd.query()
+                        for job in results:
+                            if job['ClusterId'] == job_id:
+                                return str(job)
+                    else:
+                        #TODO: return an error because job_id has not been supplied but GET is for querying jobs
+                        pass
+            else:
+                # TODO: return error for no user
+                pass
         except:
             cherrypy.request.app.log.error('Exception on JobsResouce.index', traceback=True)
 

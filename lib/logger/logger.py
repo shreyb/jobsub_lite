10f33b2254cfbdb9c9de1775379085f048c72b6c
Nick Palombo <npalombo@gmail.com>
2013-11-21 12:26:49 -0600

    bug
    Signed-off-by: Nick Palombo <npalombo@gmail.com>

--- lib/logger/logger.py
+++ lib/logger/logger.py
@@ -4,6 +4,6 @@ import cherrypy
 
 def log(msg='', context='', severity=logging.INFO, traceback=False):
     try:
-        logger.log(msg, context, severity, traceback)
+        cherrypy.request.app.log.error(msg, context, severity, traceback)
     except:
         logging.log(severity, msg)

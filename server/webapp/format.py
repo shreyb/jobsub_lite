f82b851d927f2439c3cfe5270dfecdbb78c1f5e1
Nick Palombo <npalombo@gmail.com>
2013-11-21 14:38:25 -0600

    formatting
    Signed-off-by: Nick Palombo <npalombo@gmail.com>

--- server/webapp/format.py
+++ server/webapp/format.py
@@ -4,15 +4,26 @@ import json
 from pprint import pformat
 
 
-def htmlPrintItems(dictObj, indent=0):
+def htmlPrintItem(obj, indent):
+    s = ''
+    if isinstance(obj, dict):
+        for k, v in obj.iteritems():
+            s = '%s %s %s %s %s %s' % (s, '  ' * indent, '<li>', str(k), ':', '</li>')
+            s = '%s %s' % (s, htmlPrintItem(v, indent + 1))
+    elif isinstance(obj, list):
+        s = '%s %s' % (' '*indent, '<ul>\n')
+        for v in obj:
+            s = '%s %s' % (s, htmlPrintItem(v, indent + 1))
+        s = '%s %s %s' % (s, '  ' * indent, '</ul>\n')
+    else:
+        s = '%s %s %s %s %s' % (s, ' ' * indent, '<li>', str(obj), '</li>')
+    return s
+
+
+def htmlPrintItemList(obj, indent=0):
     s = '%s %s' % (' '*indent, '<ul>\n')
-    for k, v in dictObj.iteritems():
-        if isinstance(v, dict):
-            s = '%s %s %s %s %s %s' % (s, '  '*indent, '<li>', str(k), ':', '</li>')
-            s = '%s %s' % (s, htmlPrintItems(v, indent+1))
-        else:
-            s = '%s %s %s %s %s %s %s' % (s, ' '*indent, '<li>', str(k), ':', str(v), '</li>')
-    s = '%s %s %s' % (s, '  '*indent, '</ul>\n')
+    s = '%s %s' % (s, htmlPrintItem(obj, indent))
+    s = '%s %s %s' % (s, '  ' * indent, '</ul>\n')
     return s
 
 
@@ -26,7 +37,7 @@ def format_response(content_type, data):
         return str(pformat(data))
     elif 'text/html' in content_type_list:
         cherrypy.response.headers['Content-Type'] = 'text/html'
-        return '<html><body>%s</body></html>' % htmlPrintItems(data)
+        return '<html><body>%s</body></html>' % htmlPrintItemList(data)
     else:
         return 'Content type %s not supported' % content_type
 

3e973c7de3c1a61ca150919ef2e9c424215a96ec
Nick Palombo <npalombo@gmail.com>
2013-11-21 09:58:34 -0600

    setting the right script name
    Signed-off-by: Nick Palombo <npalombo@gmail.com>

--- server/webapp/jobsub_api.py
+++ server/webapp/jobsub_api.py
@@ -207,17 +207,28 @@ root.accountinggroups = AccountingGroupsResource()
 
 def application(environ, start_response):
     os.environ['JOBSUB_INI_FILE'] = environ['JOBSUB_INI_FILE']
-    cherrypy.tree.mount(root, script_name='', config=None)
+
+    script_name = ''
+    appname = environ['JOBSUB_APP_NAME']
+    if appname is not None:
+        script_name = os.path.join('/', appname)
+        version = environ['JOBSUB_VERSION']
+        if version is not None:
+            script_name = os.path.join(script_name, appname)
+    cherrypy.tree.mount(root, script_name=script_name, config=None)
+
     log_dir = environ['JOBSUB_LOG_DIR']
     mkdir_p(log_dir)
     access_log = os.path.join(log_dir, 'access.log')
     error_log = os.path.join(log_dir, 'error.log')
+
     cherrypy.config.update({
         'environment': 'embedded',
         'lob.screen': False,
         'log.error_file': error_log,
         'log.access_file': access_log
     })
+
     return cherrypy.tree(environ, start_response)
 
 if __name__ == '__main__':

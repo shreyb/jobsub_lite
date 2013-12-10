4a441e390c55323024f275f422422d5e9ec52a2e
root <root@fermicloud348.fnal.gov>
2013-12-10 14:25:24 -0600

    Fixed bug in script

--- packaging/package.sh
+++ packaging/package.sh
@@ -5,8 +5,8 @@ echo "%_tmppath /tmp" >> ~/.rpmmacros
 mkdir -p ~/rpm/BUILD ~/rpm/RPMS ~/rpm/SOURCES ~/rpm/SPECS ~/rpm/SRPMS
 cp ./jobsub/packaging/jobsub_server.spec ~/rpm/SPECS
 mv ./jobsub ./jobsub-1.0
-ln -s ./jobsub-1.0 ./jobsub
 tar --exclude="*.pyc" --exclude="*client*" --exclude=".*" --exclude="doc" --exclude="packaging" --exclude="*.log" --exclude="*.sock" --exclude="dev_use_virtual_env.sh" --exclude="Readme" --exclude="setup" --exclude="*.pyo" --exclude="requirements.txt" -cf jobsub-1.0.tar -v jobsub-1.0
+mv ./jobsub-1.0 ./jobsub
 gzip jobsub-1.0.tar
 mv jobsub-1.0.tar.gz ~/rpm/SOURCES/
 rpmbuild -bb ~/rpm/SPECS/jobsub_server.spec

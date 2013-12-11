3343396ebeba48179e65cdf4b9f0b7f232a1dfe9
Nick Palombo <npalombo@gmail.com>
2013-12-11 13:04:07 -0600

    Change to 0.1
    Signed-off-by: Nick Palombo <npalombo@gmail.com>

--- packaging/package.sh
+++ packaging/package.sh
@@ -4,11 +4,11 @@ echo "%_topdir ${HOME}/rpm" > ~/.rpmmacros
 echo "%_tmppath /tmp" >> ~/.rpmmacros
 mkdir -p ~/rpm/BUILD ~/rpm/RPMS ~/rpm/SOURCES ~/rpm/SPECS ~/rpm/SRPMS
 cp ./jobsub/packaging/jobsub_server.spec ~/rpm/SPECS
-mv ./jobsub ./jobsub-1.0
-tar --exclude="*.pyc" --exclude="*client*" --exclude=".*" --exclude="doc" --exclude="packaging" --exclude="*.log" --exclude="*.sock" --exclude="dev_use_virtual_env.sh" --exclude="Readme" --exclude="setup" --exclude="*.pyo" --exclude="requirements.txt" -cf jobsub-1.0.tar -v jobsub-1.0
-mv ./jobsub-1.0 ./jobsub
-gzip jobsub-1.0.tar
-mv jobsub-1.0.tar.gz ~/rpm/SOURCES/
+mv ./jobsub ./jobsub-0.1
+tar --exclude="*.pyc" --exclude="*client*" --exclude=".*" --exclude="doc" --exclude="packaging" --exclude="*.log" --exclude="*.sock" --exclude="dev_use_virtual_env.sh" --exclude="Readme" --exclude="setup" --exclude="*.pyo" --exclude="requirements.txt" -cf jobsub-0.1.tar -v jobsub-0.1
+mv ./jobsub-0.1 ./jobsub
+gzip jobsub-0.1.tar
+mv jobsub-0.1.tar.gz ~/rpm/SOURCES/
 rpmbuild -bb ~/rpm/SPECS/jobsub_server.spec
-cp ~/rpm/RPMS/noarch/jobsub-1.0-0.noarch.rpm ./jobsub/packaging
+cp ~/rpm/RPMS/noarch/jobsub-0.1-0.noarch.rpm ./jobsub/packaging
 # TODO: the RPM has to be put someplace that the server admin can download it. Probably the yum repo.

ff335f501de33391f35b49db8ac3d0943244c37c
Dennis Box <dbox@fnal.gov>
2013-12-19 14:35:05 -0600

    v1_2o

--- jobsub_tools/prd/jobsub_tools/v1_1/Linux-2/pylib/groupsettings/__init__.py
+++
@@ -1,7 +0,0 @@
-# $Id$
-from JobSettings import JobSettings
-from LbneSettings import LbneSettings
-from MinervaSettings import MinervaSettings
-from MinosSettings import MinosSettings
-from NovaSettings import NovaSettings
-#from JobUtils import JobUtils

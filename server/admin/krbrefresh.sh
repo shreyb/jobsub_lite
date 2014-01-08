b66efcf497a32482d4aadd396fd994788f7b6cc0
Dennis Box <dbox@fnal.gov>
2014-01-08 16:33:26 -0600

    check running as right user

--- server/admin/krbrefresh.sh
+++ server/admin/krbrefresh.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-
+AUTHORIZED_USER=`grep 'WSGIDaemonProcess jobsub' /etc/httpd/conf.d/jobsub_api.conf | sed s/^.*user=// | sed 's/\ .*$//'`
+ME=`whoami`
+if [ "$AUTHORIZED_USER" != "$ME" ]; then
+    echo "this script must be run as user $AUTHORIZED_USER"
+    echo "you are running as $ME"
+    echo "exiting...."
+    exit 1
+fi
 export PYTHONPATH=/opt/jobsub/lib/logger/:/opt/jobsub/lib/JobsubConfigParser/:/opt/jobsub/server/webapp
 grep  SetEnv /etc/httpd/conf.d/jobsub_api.conf | sed s/SetEnv/export/ | sed 's/\([A-Z]\)\ /\1=/' > /tmp/jobsub_admin_env.sh
 source /tmp/jobsub_admin_env.sh

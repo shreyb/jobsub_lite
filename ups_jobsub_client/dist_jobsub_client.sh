89a6105a89b2acbb544c93196f09eccb90353581
Dennis Box <dbox@novagpvm01.fnal.gov>
2014-01-21 17:12:04 -0600

    scripts to download v0.1.2 client tarball, and repackage it as ups product 'jobsub_client' in the /fnal/ups/etc of the target machine. ex: ups_jobsub_client/dist_jobsub_client.sh novagpvm01 will install jobsub_client on novagpvm01.

---
+++ ups_jobsub_client/dist_jobsub_client.sh
@@ -0,0 +1,31 @@
+#!/bin/sh 
+VERS=v0_1_2
+REV=''
+
+if [ "$1" ==  "" ]; then
+	echo "usage $0 target_machine"
+	echo "tars up jobsub_client and distributes it to /fnal/ups/prd"
+	exit -1
+fi
+
+./make_tablefile.py $VERS$REV
+cd ups_db
+tar cvf db.jobsub_client.tar jobsub_client 
+scp db.jobsub_client.tar products@$1.fnal.gov:/fnal/ups/db
+ssh products@$1.fnal.gov "cd /fnal/ups/db;  tar xvf db.jobsub_client.tar; rm db.jobsub_client.tar; "
+rm  db.jobsub_client.tar
+cd -
+wget https://cdcvs.fnal.gov/redmine/attachments/download/15054/jobsub-client-v0.1.2.tar.gz
+tar xzvf jobsub-client-v0.1.2.tar.gz
+rm jobsub-client-v0.1.2.tar.gz
+mv jobsub/client jobsub/jobsub_client
+cd jobsub
+tar cvf prd.jobsub_client.tar jobsub_client 
+
+scp prd.jobsub_client.tar products@$1.fnal.gov:/fnal/ups/prd
+CMD="cd /fnal/ups/prd; mkdir -p jobsub_client/$VERS$REV; rm -rf jobsub_client/$VERS$REV/* ; mkdir -p tmp; cd tmp; rm -rf *; tar xvf ../prd.jobsub_client.tar ; cd ..; mv tmp/jobsub_client jobsub_client/$VERS$REV/NULL; chmod -R g+w jobsub_client/$VERS$REV;rm prd.jobsub_client.tar; rm -rf tmp "
+echo "performing $CMD on $1"
+ssh products@$1.fnal.gov $CMD 
+rm prd.jobsub_client.tar
+cd ..
+
---
+++ ups_jobsub_client/make_tablefile.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import os,sys,datetime,string
+
+version_template="""
+FILE = version
+PRODUCT = jobsub_client
+VERSION = %s
+
+#*************************************************
+#
+FLAVOR = NULL
+QUALIFIERS = ""
+  DECLARER = products
+  DECLARED = 2010-08-26 16.01.33 GMT
+  MODIFIER = products
+  MODIFIED = 2010-08-26 16.01.33 GMT
+  PROD_DIR = jobsub_client/%s/NULL
+  UPS_DIR = ups
+  TABLE_FILE = %s.table
+
+"""
+
+table_template="""
+FILE=TABLE
+PRODUCT=jobsub_client
+
+GROUP:
+
+  FLAVOR=ANY
+  QUALIFIERS=""
+
+
+COMMON:
+
+  ACTION=SETUP
+    setupenv()
+    proddir()
+    setupRequired(python v2_7_3)
+    setupRequired(pycurl 7_15_5)
+    envPrepend(PYTHONPATH, ${UPS_PROD_DIR} )
+    envPrepend(PATH, ${UPS_PROD_DIR})
+
+
+END:
+
+GROUP:
+  FLAVOR=ANY
+  QUALIFIERS="build"
+
+COMMON:
+
+  ACTION=SETUP
+    envSet(JOBSUB_TOOLS_DIR, ${UPS_PROD_DIR})
+    envPrepend(LD_LIBRARY_PATH, ${UPS_PROD_DIR}/lib/, ':' )
+    envPrepend(PYTHONPATH, ${UPS_PROD_DIR}, ':' )
+    envPrepend(PATH, ${UPS_PROD_DIR})
+
+END:
+
+"""
+
+current_template="""
+FILE = chain
+PRODUCT = jobsub_client
+CHAIN = current
+
+#*************************************************
+#
+FLAVOR = NULL
+VERSION = %s
+QUALIFIERS = ""
+  DECLARER = products
+  DECLARED = 2010-08-26 16.01.33 GMT
+  MODIFIER = products
+  MODIFIED = 2010-08-26 16.01.33 GMT
+"""
+
+if __name__ == "__main__":
+
+    vers=sys.argv[1]
+    
+    f=open("ups_db/jobsub_client/%s.table"%vers, 'w')
+    f.write(table_template)
+    f.close()
+
+    version=version_template % (vers,vers,vers)
+    f=open("ups_db/jobsub_client/%s.version"%vers, 'w')    
+    f.write(version)
+    f.close()
+
+    current=current_template % (vers)
+    f=open("ups_db/jobsub_client/current.chain", 'w')
+    f.write(current)
+    f.close()
+ 

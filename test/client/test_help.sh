55069b01cc3b341dd4ddc668e6fa237eaf0dc296
Dennis Box <dbox@fnal.gov>
2014-02-04 16:52:59 -0600

    forgot to checkin this updated test

--- test/client/test_help.sh
+++ test/client/test_help.sh
@@ -5,5 +5,12 @@ if [ "$1" = "" ]; then
     echo "test that jobsub help is implemented on server"
     exit 0
 fi
-MACH=$1
+source ./setup_env.sh
+export MACH=$1
+shift
+
+export SERVER=https://${MACH}:8443
+
 curl -k  --cert /tmp/x509up_u${UID}  -H "Accept: text/html" -X GET https://${MACH}:8443/jobsub/acctgroups/${GROUP}/help/
+
+$EXEPATH/jobsub_submit.py --jobsub-server=$SERVER --group $GROUP --help

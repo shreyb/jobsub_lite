816ff5e426c55c41f6f7c763ee67012106fd46e0
Parag Mhashilkar <parag@fnal.gov>
2014-03-28 21:41:44 -0500

    Tools to help make release packages

---
+++ packaging/ReleaseManagerLib.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+
+import os, sys
+import subprocess
+import shlex
+import shutil
+import popen2
+import traceback
+import string
+#from pylint import lint
+
+
+
+
+class ExeError(RuntimeError):
+
+    def __init__(self,str):
+        RuntimeError.__init__(self,str)
+
+
+class Release:
+
+    def __init__(self, product, ver, rpm_rel, rc, srcDir, relDir='/tmp'):
+        self.product = product
+        self.version = ver
+        self.rc = rc
+        self.rpmRelease = rpm_rel
+
+        self.clientVersion = self.version
+        self.serverRPMVersion = self.version
+        self.serverRPMRelease = self.rpmRelease
+
+        if self.rc:
+            self.clientVersion = '%s.rc%s' % (self.version, self.rc)
+            self.serverRPMRelease = '0.%s.rc%s' % (self.rpmRelease, self.rc)
+
+        self.versionString = self.clientVersion
+
+        self.sourceDir = srcDir
+        self.releaseDir = os.path.join(relDir, self.versionString)
+        self.rpmbuildDir = os.path.join(self.releaseDir, 'rpmbuild')
+        self.tasks = []
+
+    def addTask(self, task):
+        self.tasks.append(task)
+
+    def executeTasks(self):
+        for i in range(0, len(self.tasks)):
+            task = self.tasks[i]
+            print "************* Executing %s *************" % (self.tasks[i]).name
+            task.execute()
+            print "************* %s Status %s *************" % ((self.tasks[i]).name, (self.tasks[i]).status)
+
+    def printReport(self):
+        print 35*"_"
+        print "TASK" + 20*" " + "STATUS"
+        print 35*"_"
+        for i in range(0, len(self.tasks)):
+            print "%-23s %s" % ((self.tasks[i]).name, (self.tasks[i]).status)
+        print 35*"_"
+
+
+class TaskRelease:
+
+    def __init__(self, name, rel):
+        self.name = name
+        self.release = rel
+        self.status = 'INCOMPLETE'
+    #   __init__
+
+    def execute(self):
+        raise ExeError('Action execute not implemented for task %s' % self.name)
+    #   execute
+
+
+class TaskClean(TaskRelease):
+
+    def __init__(self, rel):
+        TaskRelease.__init__(self, 'Clean', rel)
+    #   __init__
+
+    def execute(self):
+        cmd = 'rm -rf %s' % self.release.releaseDir
+        execute_cmd(cmd)
+        self.status = 'COMPLETE'
+    #   execute
+
+
+class TaskSetupReleaseDir(TaskRelease):
+
+    def __init__(self, rel):
+        TaskRelease.__init__(self, 'SetupReleaseDir', rel)
+    #   __init__
+
+    def execute(self):
+        create_dir(self.release.releaseDir)
+        create_dir(self.release.rpmbuildDir)
+        self.status = 'COMPLETE'
+    #   execute
+
+
+class TaskTar(TaskRelease):
+
+    def __init__(self, rel):
+        TaskRelease.__init__(self, 'JobSubTar', rel)
+        self.excludes = PackageExcludes()
+        self.releaseFilename = 'glideinWMS_%s.tgz' % self.release.version
+        self.excludePattern = self.excludes.commonPattern
+        self.tarExe = which('tar')
+    #   __init__
+
+    def execute(self):
+        exclude = ""
+        if len(self.excludePattern) > 0:
+            exclude = "--exclude='" +  string.join(self.excludePattern, "' --exclude='") + "'"
+        #cmd = 'cd %s/..; /bin/tar %s -czf %s/%s glideinwms' % \
+        #      (self.release.sourceDir, exclude, self.release.releaseDir, self.releaseFilename)
+        src_dir = '%s/../src/%s' % (self.release.releaseDir,
+                                    self.release.version)
+        cmd = 'rm -rf %s; mkdir -p %s; cp -r %s %s/%s; cd %s; %s %s -czf %s/%s %s' % \
+              (src_dir, src_dir, self.release.sourceDir, src_dir, self.releaseDirname, src_dir, self.tarExe, exclude, self.release.releaseDir, self.releaseFilename, self.releaseDirname)
+        print "%s" % cmd
+        execute_cmd(cmd)
+        self.status = 'COMPLETE'
+    #   execute
+
+
+class TaskClientTar(TaskTar):
+
+    def __init__(self, rel):
+        TaskTar.__init__(self, rel)
+        self.name = 'JobSubClientTar'
+        self.releaseDirname = 'jobsub'
+        self.releaseFilename = 'jobsub-client-v%s.tgz' % self.release.clientVersion
+        self.excludePattern = self.excludes.clientPattern
+    #   __init__
+
+class TaskServerRPM(TaskTar):
+
+    def __init__(self, rel):
+        TaskTar.__init__(self, rel)
+        self.name = 'JobSubServerRPM'
+        self.releaseDirname = 'jobsub-%s' % self.release.serverRPMVersion
+        self.releaseFilename = 'jobsub-%s.tar.gz' % self.release.serverRPMVersion
+        self.excludePattern = self.excludes.serverPattern
+    #   __init__
+
+
+    def execute(self):
+        # First build the source tarball
+        TaskTar.execute(self)
+
+        rpm_dirs = ['BUILD', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS']
+        rpm_src_dir = os.path.join(self.release.rpmbuildDir, 'SOURCES')
+        rpm_specs_dir = os.path.join(self.release.rpmbuildDir, 'SPECS')
+        rpm_file = os.path.join(self.release.rpmbuildDir, 'RPMS', 'noarch',
+                                'jobsub-%s-%s.noarch.rpm' % (self.release.serverRPMVersion, self.release.serverRPMRelease))
+        rpm_specs_infile = os.path.join(self.release.sourceDir, 'packaging', 'jobsub_server.spec')
+        rpm_specs_outfile = os.path.join(rpm_specs_dir, 'jobsub_server.spec')
+
+        # Create directories required by rpmbuild
+        for dir in rpm_dirs:
+            create_dir(os.path.join(self.release.rpmbuildDir, dir))
+
+        # Move the source tarball in place
+        shutil.move(os.path.join(self.release.releaseDir, self.releaseFilename),
+                    rpm_src_dir)
+
+        # Create the spec file
+        #shutil.copyfile(rpm_specs_file, os.path.join(rpm_specs_dir, os.path.basename(rpm_specs_file)))
+        fd = open(rpm_specs_infile, 'r')
+        specs = fd.readlines()
+        fd.close()
+        
+        fd = open(rpm_specs_outfile, 'w')
+        for line in specs:
+            line = line.replace('__VERSION__', self.release.serverRPMVersion)
+            line = line.replace('__RELEASE__', self.release.serverRPMRelease)
+            fd.write(line)
+        fd.close()
+
+        # Create rpmmacros file
+        rpm_macros_file = os.path.join(os.path.expanduser('~'), '.rpmmacros')
+        fd = open(rpm_macros_file, 'w')
+        fd.write('%%_topdir %s\n' % self.release.rpmbuildDir)
+        fd.write('%%_tmppath %s\n' % '/tmp')
+        fd.close()
+
+        # Create the rpm
+        cmd = 'rpmbuild -bb %s' % rpm_specs_outfile
+        print "%s" % cmd
+        execute_cmd(cmd)
+        rpm_filename = os.path.basename(rpm_file)
+        shutil.copyfile(rpm_file, os.path.join(self.release.releaseDir, rpm_filename))
+
+
+class PackageExcludes:
+
+    def __init__(self):
+
+        self.commonPattern = [
+            'CVS',
+            '.git*',
+            '.DS_Store',
+        ]
+
+        # Patterns that need to be excluded from the client tarball
+        self.clientPattern = [
+            'CVS',
+            '.git*',
+            '.DS_Store',
+            'client/*.pyc',
+            'config',
+            'jobsub_tools',
+            'lib',
+            'packaging',
+            'server',
+            'test',
+            'ups_jobsub_client',
+        ]
+
+        # Patterns that need to be excluded from the server tarball
+        self.serverPattern = [
+            'CVS',
+            '.git*',
+            '.t*',
+            '.DS_Store',
+            'client',
+            'config',
+            'jobsub_tools',
+            'packaging',
+            'test',
+            'ups_jobsub_client',
+            '*.pyc',
+            '*.pyo',
+            'doc',
+            '*.log',
+            '*.sock',
+            'setup',
+            'Readme',
+            'dev_use_virtual_env.sh',
+            'requirements.txt',
+        ]
+############################################################
+#
+# P R I V A T E, do not use
+#
+############################################################
+
+def create_dir(dir, mode=0755, errorIfExists=False):
+    try:
+        os.makedirs(dir, mode=0755)
+    except OSError as (errno, stderror):
+        if (errno == 17) and (errorIfExists == False):
+            print 'Dir already exists reusing %s' % dir
+        else:
+            raise
+    except Exception:
+            raise
+
+# can throw ExeError
+def execute_cmd(cmd, stdin_data=None):
+    child = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if stdin_data!=None:
+        child.stdin.write(stdin_data)
+
+    tempOut = child.stdout.readlines()
+    tempErr = child.stderr.readlines()
+    child.communicate()
+    #child.childerr.close()
+    try:
+        errcode = child.wait()
+    except OSError, e:
+        if len(tempOut) != 0:
+            # if there was some output, it is probably just a problem of timing
+            # have seen a lot of those when running very short processes
+            errcode = 0
+        else:
+            raise ExeError, "Error running '%s'\nStdout:%s\nStderr:%s\nException OSError: %s"%(cmd,tempOut,tempErr,e)
+    if (errcode != 0):
+        raise ExeError, "Error running '%s'\ncode %i:%s"%(cmd,errcode,tempErr)
+    return tempOut
+
+"""
+def execute_cmd1(cmd, stdin_data=None):
+    #child = subprocess.Popen(shlex.split(cmd), stdin=stdin_data)
+    child=popen2.Popen3(cmd,True)
+    if stdin_data!=None:
+        child.tochild.write(stdin_data)
+    child.tochild.close()
+    tempOut = child.fromchild.readlines()
+    child.fromchild.close()
+    tempErr = child.childerr.readlines()
+    child.childerr.close()
+    try:
+        errcode = child.wait()
+    except OSError, e:
+        if len(tempOut) != 0:
+            # if there was some output, it is probably just a problem of timing
+            # have seen a lot of those when running very short processes
+            errcode = 0
+        else:
+            raise ExeError, "Error running '%s'\nStdout:%s\nStderr:%s\nException OSError: %s"%(cmd,tempOut,tempErr,e)
+    if (errcode != 0):
+        raise ExeError, "Error running '%s'\ncode %i:%s"%(cmd,errcode,tempErr)
+    return tempOut
+"""
+
+def which(program):
+    """
+    Implementation of which command in python.
+
+    @return: Path to the binary
+    @rtype: string
+    """
+
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
---
+++ packaging/release.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import getopt
+import optparse
+
+sys.path.append(os.path.join(sys.path[0], '../lib'))
+import ReleaseManagerLib
+
+def usage():
+    print "%s <version> <SourceDir> <ReleaseDir>" % os.path.basename(sys.argv[0])
+    print "Example: %s v2.5.3 /tmp/glideinwms /tmp/release_dir" % os.path.basename(sys.argv[0])
+
+def parse_opts(argv):
+    parser = optparse.OptionParser(usage='%prog [options]',
+                                   version='v0.1',
+                                   conflict_handler="resolve")
+    parser.add_option('--version',
+                      dest='version',
+                      action='store',
+                      metavar='<version>',
+                      help='version to release')
+    parser.add_option('--rc',
+                      dest='rc',
+                      default=None,
+                      action='store',
+                      metavar='<Release Candidate Number>',
+                      help='Release Candidate')
+    parser.add_option('--rpm-release',
+                      dest='rpmRel',
+                      default=1,
+                      action='store',
+                      metavar='<RPM Release Number>',
+                      help='RPM Release Number')
+    parser.add_option('--source-dir',
+                      dest='srcDir',
+                      action='store',
+                      metavar='<source directory>',
+                      help='directory containing the source code')
+    parser.add_option('--release-dir',
+                      dest='relDir',
+                      action='store',
+                      metavar='<release directory>',
+                      help='directory to store release tarballs and webpages')
+
+    if len(argv) < 5:
+        print "ERROR: Insufficient arguments specified"
+        parser.print_help()
+        sys.exit(1)
+    options, remainder = parser.parse_args(argv)
+    if len(remainder) > 1:
+        parser.print_help(file)
+    if not required_args_present(options):
+        print "ERROR: Missing required arguments"
+        parser.print_help()
+        sys.exit(1)
+    return options
+
+def required_args_present(options):
+    try:
+        if not (options.version and options.srcDir and options.relDir):
+            return False
+    except AttributeError:
+        return False
+    return True
+#   check_required_args
+
+
+#def main(ver, srcDir, relDir):
+def main(argv):
+    options = parse_opts(argv)
+    #sys.exit(1)
+    product = 'jobsub'
+    ver = options.version
+    rpm_rel = options.rpmRel
+    rc = options.rc
+    
+    srcDir = options.srcDir
+    relDir = options.relDir
+    
+    print "___________________________________________________________________"
+    print "Creating following release"
+    print "Product=%s Version=%s RC=%s RPMRelease=%s\nSourceDir=%s\nReleaseDir=%s" % (product, ver, rc, rpm_rel, srcDir, relDir)
+    print "___________________________________________________________________"
+    print
+    rel = ReleaseManagerLib.Release(product, ver, rpm_rel, rc, srcDir, relDir)
+
+    rel.addTask(ReleaseManagerLib.TaskClean(rel))
+    rel.addTask(ReleaseManagerLib.TaskSetupReleaseDir(rel))
+    rel.addTask(ReleaseManagerLib.TaskClientTar(rel))
+    rel.addTask(ReleaseManagerLib.TaskServerRPM(rel))
+
+    rel.executeTasks()
+    rel.printReport()
+
+if __name__ == "__main__":
+    main(sys.argv)

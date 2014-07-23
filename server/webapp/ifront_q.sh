b935f92ffef265ea355f95a5ea49c67c22131f6b
Dennis D. Box <dbox@fnal.gov>
2014-07-23 15:46:55 -0500

    much improved ifront_q for HA system

--- server/webapp/ifront_q.sh
+++ server/webapp/ifront_q.sh
@@ -1,40 +1,30 @@
 #!/bin/sh
 
-VERSION=20120229
 
 TMPFILE=/tmp/condor_q_`date +%Y%m%d_%H%M%S`_$$
-condor_q > ${TMPFILE}
+condor_q -g | sed '/Schedd:/d' | sed '/ID.*OWNER/d' | sed '/^$/d' > ${TMPFILE}
 
-TMPFILETRIM=/tmp/condor_q_`date +%Y%m%d_%H%M%S`_${$}_trim
-LINES=`cat ${TMPFILE} | wc -l`
-TOHEAD=$(( $LINES - 2 ))
-TOTAIL=$(( $LINES - 6 ))
-cat ${TMPFILE} | head -$TOHEAD | tail -$TOTAIL > ${TMPFILETRIM}
 
-OWNERLIST="`cat ${TMPFILETRIM} | awk '{if ($2!="gfactory") print $2}' | sort | uniq `"
-HEADER=`head -3 ${TMPFILE} | tail -1 | sed 's/-- Submitter: /-- Summary of /'`
-
+OWNERLIST="`cat ${TMPFILE} | awk '{print $2}' | sort | uniq `"
 # output
 echo 
-echo "$HEADER"
 echo " OWNER        RUN   IDLE   HELD     OLDEST_JOB"
 for OWNER in $OWNERLIST
 do
-  STATUS=`cat ${TMPFILETRIM} | grep $OWNER | grep -v condor_dagman | awk '{if ($6=="I") i++;if ($6=="H") h++;if ($6=="R") r++;printf "%-10s %6d %6d %6d \n", $2, r, i, h}' | tail -1`
-  FIRST=`cat ${TMPFILETRIM} | grep $OWNER | head -1 | awk '{printf "%s %s %s %-18s\n",$3,$4,$5,$NF}'`
+  STATUS=`cat ${TMPFILE} | grep $OWNER | awk '{if ($6=="I") i++;if ($6=="H") h++;if ($6=="R") r++;printf "%-10s %6d %6d %6d \n", $2, r, i, h}' | tail -1`
+  FIRST=`cat ${TMPFILE} | grep $OWNER | head -1 | awk '{printf "%s %s %s %-18s\n",$3,$4,$5,$NF}'`
   echo "$STATUS    $FIRST"
 done
-TOTALS=`cat ${TMPFILETRIM} | grep -v gfactory | grep -v condor_dagman | awk '{if ($6=="I") i++;if ($6=="H") h++;if ($6=="R") r++;printf "%-10s %6d %6d %6d \n", "TOTALS", r, i, h}' | tail -1`
+TOTALS=`cat ${TMPFILE} |  awk '{if ($6=="I") i++;if ($6=="H") h++;if ($6=="R") r++;printf "%-10s %6d %6d %6d \n", "TOTALS", r, i, h}' | tail -1`
 echo
 echo "$TOTALS"
 
-# gfactory
-OWNER=gfactory
-STATUS=`cat ${TMPFILETRIM} | grep $OWNER | awk '{if ($6=="I") i++;if ($6=="H") h++;if ($6=="R") r++;printf "Farm glideins:  R=%d I=%d H=%d\n", r, i, h}' | tail -1`
+# 
+RUNNING=`condor_status -claimed | grep glidein | wc -l`
+AVAIL=`condor_status -avail | grep glidein | grep -i unclaimed | wc -l`
 echo
-echo "$STATUS"
+echo "glidein count: $RUNNING currently servicing jobs, $AVAIL unclaimed "
 echo
 
 rm ${TMPFILE}
-rm ${TMPFILETRIM}
 

dac1e70d89654c6e35270ea547a3028ffb574e03
Dennis Box <dbox@fnal.gov>
2014-10-16 18:34:46 -0500

    add JOBSUBPARENTJOBID if DAG is submitted from dagNabbit. Also fixed hardcoded path in test, changing this revealed bug in client where it only worked with hardcoded paths in dag description file

--- test/client/test_dag_submit.sh
+++ test/client/test_dag_submit.sh
@@ -4,10 +4,11 @@ source ./setup_env.sh
 
 export SERVER=https://${MACH}:8443
 
+cd jobsubDagTest
 $EXEPATH/jobsub_submit_dag --group $GROUP \
---debug $SERVER_SPEC  file://`pwd`/jobsubDagTest/dagTest
+--debug $SERVER_SPEC  file://dagTest
 T1=$?
-
+cd -
 echo $0 exiting with status $T1
 exit $T1
 

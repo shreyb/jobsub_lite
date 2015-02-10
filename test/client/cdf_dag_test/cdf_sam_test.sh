0ffff23d4c6a21cc3ecee9b26900d19e3896f1d4
Dennis Box <dbox@fnal.gov>
2015-02-10 12:47:05 -0600

    didn't check in test either

--- test/client/cdf_dag_test/cdf_sam_test.sh
+++ test/client/cdf_dag_test/cdf_sam_test.sh
@@ -55,7 +55,7 @@ gCMD2="$EXEPATH/jobsub_submit $SUBMIT_FLAGS \
     -e SAM_GROUP \
     -e SAM_USER \
     -e SAM_DATASET \
-    -e SAM_PROJECT22 \
+    -e SAM_PROJECT2 \
     -e IFDH_BASE_URI \
     -G cdf $RESOURCE_PROVIDES \
     -N 3 --generate-email-summary \
@@ -121,7 +121,25 @@ $gCMD
 T3=$?
 echo exit status of last command $T3
 
-! (( $T0 || $T1 || $T2 || $T3 ))
+gCMD="$EXEPATH/jobsub_submit $SUBMIT_FLAGS \
+    --debug
+    -e IFDH_BASE_URI \
+    -G cdf $RESOURCE_PROVIDES \
+    -N 3 --generate-email-summary \
+    --mail_on_error --maxParallelSec 5 \
+    $SERVER_SPEC \
+    --tarFile=dropbox://input3.tgz \
+     some_subdir/testNoSAM.sh $ foo bar baz"
+echo ======================================================
+echo $gCMD
+echo ======================================================
+
+$gCMD
+
+T4=$?
+echo exit status of last command $T4
+
+! (( $T0 || $T1 || $T2 || $T3  || $T4 ))
 
 TF=$?
 exit $TF

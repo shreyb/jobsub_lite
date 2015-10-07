502e5fa1db2cc2f11c3242222f56381adcae1ab1
Dennis D. Box <dbox@fnal.gov>
2015-10-06 23:08:58 -0500

    added a new test to verify more complicated dags work cleanup as suggested by pylint

--- test/client/test_dag_submit.sh
+++ test/client/test_dag_submit.sh
@@ -8,8 +8,14 @@ cd jobsubDagTest
 $EXEPATH/jobsub_submit_dag   $GROUP_SPEC \
 --debug $SERVER_SPEC  file://dagTest  --maxConcurrent 3
 T1=$?
+echo T1=$T1
+$EXEPATH/jobsub_submit_dag   $GROUP_SPEC \
+--debug $SERVER_SPEC  file://dagTest2  
+T2=$?
+echo T2=$T2
 cd -
-TFINAL=$T1
+! (( $T1 || $T2  ))
+TFINAL=$?
 echo $0 exiting with status $TFINAL
 exit $TFINAL
 

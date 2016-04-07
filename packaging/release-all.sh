a347b7c5920623445e653af33e840f63c62ece01
Dennis D. Box <dbox@fnal.gov>
2016-04-07 12:42:30 -0500

    made some changes to distribution script

--- packaging/release-all.sh
+++ packaging/release-all.sh
@@ -3,9 +3,12 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { echo "$@"; "$@" || die "FAILED $*"; }
 
+thisdir=`pwd`
+packagedir=`dirname $0`
+try cd $packagedir
 try source ./release-all.env.sh
 
-cd $HOME/jobsub
+cd ..
 if [ "$PRACTICE_RUN" = "" ]; then
     EXE=try
 else
@@ -28,21 +31,29 @@ RDIR=/var/tmp/$USER
 #tag and push to origin
 if [  "$TAG_AND_PUSH" != "" ]; then 
    TVER="v${RVER}"  
-   $EXE git tag -a -m ${TVER} ${TVER}
+   $EXE git tag  -m ${TVER} -a ${TVER}
    $EXE git push origin ${TVER}
-   $EXE git tag -a -m jobsub_tools_${JTVER} jobsub_tools_${JTVER}
+   $EXE git tag  -m jobsub_tools_${JTVER} -a jobsub_tools_${JTVER}
    $EXE git push origin jobsub_tools_${JTVER}
 fi
 
 #generate rpm and put in repo
-cd packaging
+try cd packaging
+try test -e release.py
 $EXE ./release.py  --version ${VER} --source-dir $HOME/jobsub --release-dir ${RDIR}  ${RC_CMD} 
-RPM=`ls ${RDIR}/${RVER}/*.rpm`
+RPM=`ls ${RDIR}/${RVER}/*.rpm 2>/dev/null`
+if [ "$RPM" = "" ]; then
+    RPM=${RDIR}/${RVER}/jobsub.${RVER}.rpm
+fi
 $EXE ./populate-rpmrepo.sh $RPM $PROD 
 
 #put jobsub_client in ups/upd
-cd ../ups_jobsub_client/
-CLIENT=`ls ${RDIR}/${RVER}/*.tgz`
+try cd ../ups_jobsub_client/
+CLIENT=`ls ${RDIR}/${RVER}/*.tgz 2>/dev/null` 
+if [ "$CLIENT" = "" ]; then
+    CLIENT=${RDIR}/${RVER}/jobsub_client.${RVER}.tgz
+fi
+try test -e dist_jobsub_client.sh 
 $EXE ./dist_jobsub_client.sh ${UPSNODE} $CVER $CLIENT
 CMD="source /fnal/ups/etc/setups.sh; setup ups; setup upd; upd addproduct jobsub_client ${CVER} -f NULL"
 echo $CMD > $HOME/jobsub_client_to_ups.sh
@@ -50,10 +61,11 @@ $EXE scp $HOME/jobsub_client_to_ups.sh ${UPSNODE}:jobsub_client_to_ups.sh
 $EXE ssh ${UPSNODE} 'sh ./jobsub_client_to_ups.sh'
 
 #put jobsub_tools in ups/upd
-cd ../jobsub_tools/
+try cd ../jobsub_tools/
+try test -e dist_jobsub.sh
 $EXE ./dist_jobsub.sh ${UPSNODE} $TOOL_VER  _${JTMVER}
 CMD2="source /fnal/ups/etc/setups.sh; setup ups; setup upd; upd addproduct jobsub_tools ${JTVER}  -f Linux+2"
 echo $CMD2 > $HOME/jobsub_tools_to_ups.sh
 $EXE scp $HOME/jobsub_tools_to_ups.sh ${UPSNODE}:jobsub_tools_to_ups.sh
 $EXE ssh ${UPSNODE} 'sh ./jobsub_tools_to_ups.sh'
-
+cd $thisdir

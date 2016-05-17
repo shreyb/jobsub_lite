109cb5f8e20796dbdaf170fc34e9c27d32641e4a
Dennis D. Box <dbox@fnal.gov>
2016-05-17 14:41:07 -0500

    new settings for jenkins ci machine

--- test/client/jenkins_test_configs.sh
+++ test/client/jenkins_test_configs.sh
@@ -1,10 +1,11 @@
 source /fnal/ups/etc/setups.sh
 setup cigetcert
+setup kx509
 unset X509_USER_CERT
 unset X509_USER_KEY
 export X509_USER_PROXY=/tmp/x509up_u${UID}
 export KRB5CCNAME=`ls -lart /tmp/krb5cc_${UID}* | tail -1 | awk '{print $9}'`
-/usr/krb5/bin/kx509
+kx509
 export X509_USER_CERT=/tmp/x509up_u${UID}
 export X509_USER_KEY=/tmp/x509up_u${UID}
 unset X509_USER_PROXY

4edaba93eec072ac3c6a70d68a67b889e1a9bf45
Dennis D. Box <dbox@fnal.gov>
2016-08-11 14:09:13 -0500

    change el6 to 6

--- packaging/populate-rpmrepo.sh
+++ packaging/populate-rpmrepo.sh
@@ -20,7 +20,7 @@ repologin="flxi02.fnal.gov"
 #repodir='/var/www/html/files/jobsub'
 repodir='/web/sites/j/jobsub.fnal.gov/htdocs/rpms'
 #For now only SL6 support
-versionlist='el6'
+versionlist='6'
 #For now only 64bit support
 archlist='x86_64'
 # Create repo for dev (developers) and one for production (operations)

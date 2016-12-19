c3e2f81b5029e510abb4c7671a576ae973d74158
Dennis D. Box <dbox@fnal.gov>
2016-12-19 17:42:00 -0600

    accept DNs with apostrophes

--- server/webapp/auth.py
+++ server/webapp/auth.py
@@ -549,7 +549,7 @@ def authorize_myproxy(dn, username, acctgroup, acctrole=None ,age_limit=3600):
             child_env['X509_USER_CERT']=child_env['JOBSUB_SERVER_X509_CERT']
             child_env['X509_USER_KEY']=child_env['JOBSUB_SERVER_X509_KEY']
             dn = clean_proxy_dn(dn)
-            cmd = "%s -n -l '%s' -s %s -t 24 -o %s"%\
+            cmd = """%s -n -l '%s' -s %s -t 24 -o %s"""%\
                     (myproxy_exe, dn, myproxy_server, x509_tmp_fname)
             logger.log('%s'%cmd)
             out, err = subprocessSupport.iexe_cmd(cmd,child_env=child_env)

9bafc4d126bb09f8221818d5648adaff50a5a476
Dennis D. Box <dbox@fnal.gov>
2017-05-26 14:08:32 -0500

    utility to clean out cached tarballs

--- server/admin/jobsub_preen.sh
+++ server/admin/jobsub_preen.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-export PYTHONPATH=/opt/jobsub/lib/logger/:/opt/jobsub/lib/JobsubConfigParser/:/opt/jobsub/server/webapp
-grep  SetEnv /etc/httpd/conf.d/jobsub_api.conf | sed s/SetEnv/export/ | sed 's/\([A-Z]\)\ /\1=/' | sed 's/=[[:space:]]\+/=/' > /tmp/jobsub_preen_env.sh
-source /tmp/jobsub_preen_env.sh
-rm /tmp/jobsub_preen_env.sh
+CONFIG_FILE=/opt/jobsub/server/conf/jobsub_api.conf
+SOURCE_FILE=/tmp/jobsub_preen_env.sh
+
+grep -i SetEnv ${CONFIG_FILE} | sed -e 's/[Ss][Ee][Tt][Ee][Nn][Vv]/export/' -e 's/\([A-Z]\)\ /\1=/' -e 's/=[[:space:]]\+/=/'  > ${SOURCE_FILE}
+PYTHONPATH=$(grep -i python ${CONFIG_FILE} | grep -i path | sed -e's/.*=//')
+echo "export PYTHONPATH=${PYTHONPATH}" >> ${SOURCE_FILE}
+source ${SOURCE_FILE}
 
 /opt/jobsub/server/admin/jobsub_preen.py $@
+
+if [ $? -eq 0 ] ; then
+    rm ${SOURCE_FILE}
+fi
 

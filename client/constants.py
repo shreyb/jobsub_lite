bc8d8bfbac431b9836afb343c3bc7a244c04dad7
Dennis D. Box <dbox@fnal.gov>
2018-01-30 22:29:29 -0600

    new url for querying max dropbox size

--- client/constants.py
+++ client/constants.py
@@ -40,6 +40,7 @@ JOBSUB_JOB_SUBMIT_URL_PATTERN_WITH_ROLE = '%s/jobsub/acctgroups/%s--ROLE--%s/job
 JOBSUB_DAG_SUBMIT_URL_PATTERN_WITH_ROLE = '%s/jobsub/acctgroups/%s--ROLE--%s/jobs/dag/'
 
 JOBSUB_AUTHMETHODS_URL_PATTERN = '%s/jobsub/acctgroups/%s/authmethods/'
+JOBSUB_DROPBOX_MAX_SIZE_URL_PATTERN = '%s/jobsub/acctgroups/%s/dropboxsize/'
 
 JOBSUB_DROPBOX_LOCATION_URL_PATTERN = '%s/jobsub/acctgroups/%s/dropboxlocation/' 
 

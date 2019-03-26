5ced40eaaea11109ada0aa8e787f5f6a71952779
Dennis D. Box <dbox@fnal.gov>
2019-03-26 14:32:01 -0500

    take out user option, autopep

--- server/webapp/job_prio.py
+++ server/webapp/job_prio.py
@@ -1,9 +1,10 @@
 """Module:
-        by_user
+        job_prio
    Purpose
-        implements condor_q <username>
-        API /jobsub/acctgroups/<group_id>/jobs/prio/<prio>/user/<username>
-        API /jobsub/acctgroups/<group_id>/jobs/prio/<prio>job_id/<jobsubjobid>/
+        sets up call to  condor_userprio -p <prio> <jobid>
+        API /jobsub/acctgroups/<group_id>/jobs/setprio/<prio>job_id/<jobsubjobid>/
+   Author 
+        Dennis Box, dbox@fnal.gov
 """
 import cherrypy
 import logger
@@ -14,61 +15,63 @@ from jobsub import is_supported_accountinggroup
 from format import format_response
 
 
-@cherrypy.popargs('setprio','param','paramval')
+@cherrypy.popargs('setprio', 'param', 'paramval')
 class JobPrioResource(object):
-    """Class that implements above URLS
-       Only responds to http GET, only
-       index.html implemented
+    """Class that implements above URL
+       and eventually calls condor_prio
+       Only responds to http PUT
     """
 
     @cherrypy.expose
     @format_response
     @check_auth
-    def index(self, acctgroup,  setprio=None, param=None, paramval=None,**kwargs):
-        logger.log('setprio=%s param=%s paramval=%s kwargs=%s' % (setprio,param,paramval,kwargs))
+    def index(self, acctgroup,  setprio=None, param=None, paramval=None, **kwargs):
+        """index.html for above URL
+        parameters:
+            @acctgroup: condor accounting group derived from <group_id> in URL
+            @setprio : value to set condor priority to
+            @param: currently must be 'job_id' to signal pathway to condor_prio
+            @paramval: <jobsubjobid> that is having its priority set to '<prio>'
+        """
+        logger.log('setprio=%s param=%s paramval=%s kwargs=%s' %
+                   (setprio, param, paramval, kwargs))
         user = None
         job_id = None
         err = None
         out = None
         rc = {'out': out, 'err': err}
         try:
-            if param and paramval :
-                if param =='job_id':
-                    job_id=paramval
-
-                if param =='user':
-                    user=paramval
-                kwargs['prio'] = setprio
-                #kwargs['user'] = user
-                #kwargs['job_id'] = job_id
+            if param and paramval:
+                if param == 'job_id':
+                    job_id = paramval
 
             else:
-                err = "must supply either a job_id or user"
-                
+                err = "must supply a job_id"
+                logger.log(rc)
             logger.log('user=%s job_id=%s' % (user, job_id))
             if is_supported_accountinggroup(acctgroup):
                 if cherrypy.request.method == 'PUT':
                     # hold/release/adjust
                     rc = util.doPUT(acctgroup, user=user,
-                               job_id=job_id, **kwargs)
+                                    job_id=job_id, **kwargs)
                 else:
                     err = 'Unsupported method: %s' % cherrypy.request.method
-                    logger.log(err, severity=logging.ERROR)
+                    logger.log(rc, severity=logging.ERROR)
                     logger.log(err, severity=logging.ERROR, logfile='error')
             else:
                 # return error for unsupported acctgroup
                 err = 'AccountingGroup %s is not configured in jobsub' % acctgroup
-                logger.log(err, severity=logging.ERROR)
+                logger.log(rc, severity=logging.ERROR)
                 logger.log(err, severity=logging.ERROR, logfile='error')
         except:
             cherrypy.response.status = 500
-            err = 'Exception on AccountJobsByUserResource.index'
-            logger.log(err, severity=logging.ERROR, traceback=True)
+            err = 'Exception on JobsPrioResource.index'
+            logger.log(rc, severity=logging.ERROR, traceback=True)
             logger.log(err, severity=logging.ERROR,
                        logfile='error', traceback=True)
         if rc.get('err'):
             cherrypy.response.status = 500
         return rc
-    
+
     def default(self,  **kwargs):
         logger.log("kwargs %s" % kwargs)

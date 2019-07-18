df44636c1bf9507c23463e8b135577e80a64519b
Dennis D. Box <dbox@fnal.gov>
2019-07-18 17:57:29 -0500

    all these shell files parse jobsub_api.conf, each a little differently. made a central parser that they all use so if the config file changes we only have to change one file

--- server/admin/fill_jobsub_history.sh
+++ server/admin/fill_jobsub_history.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-export PYTHONPATH=/opt/jobsub/lib/logger/:/opt/jobsub/lib/JobsubConfigParser/:/opt/jobsub/server/webapp
-grep  SetEnv /etc/httpd/conf.d/jobsub_api.conf | sed s/SetEnv/export/ | sed 's/\([A-Z]\)\ /\1=/' | sed 's/=[[:space:]]\+/=/' > /tmp/fill_jobsub_history_env.sh
-source /tmp/fill_jobsub_history_env.sh
-rm /tmp/fill_jobsub_history_env.sh
-
+HERE=`dirname $0`
+source $HERE/config_lib.sh
+get_jobsub_env
 /opt/jobsub/server/admin/fill_jobsub_history.py $@
 exit $?
-#TODO
-#THERE IS SOME LOGIC BELOW FOR LOADING $SPOOL/history.(older_date) FILES THAT STILL NEEDS TO FIND ITS
-#WAY INTO fill_jobsub_history.py   

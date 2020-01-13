9f533c3b11896bf96e2f635756a2e533543bb483
Dennis D. Box <dbox@fnal.gov>
2020-01-13 17:49:25 -0600

    new file to detect python version and unicode size

--- ups_jobsub_client/dist_jobsub_client.sh
+++ ups_jobsub_client/dist_jobsub_client.sh
@@ -34,6 +34,8 @@ tar xzvf $TARBALL
 rm $TARBALL
 mv jobsub/client jobsub/jobsub_client
 cp jobsub/doc/release.notes jobsub/jobsub_client
+mkdir -p jobsub/jobsub_client/ups
+mkdir -p jobsub/jobsub_client/__pycache__
 cd jobsub
 tar cvf prd.jobsub_client.tar jobsub_client 
 
